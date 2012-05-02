b37f7f46883f019cd897b5d87a07ad44f4321358
Andrew Pollok <andrew@networkwebcams.com>
2012-05-02 14:08:05 +0100

    basic cucumber steps fleshed out. Assuming a Hash indexed by a [x,y] as a grid

---
+++ features/step_definitions/game_of_life_steps.rb
@@ -0,0 +1,24 @@
+Given /^the following setup$/ do |table|
+  #table is a Cucumber::Ast::Table
+  @grid = Grid.two_d_init(table.raw)
+  @height = table.raw.size
+  @width = grid.raw[0].size
+end
+
+When /^I evolve the board$/ do
+  @grid.evolve
+end
+
+Then /^the center cell should be dead$/ do
+  @grid.grid[[@height/2,@width/2]].alive?.should be_false
+end
+
+Then /^the center cell should be alive$/ do
+  @grid.grid[[@height/2,@width/2]].alive?.should be_true
+end
+
+Then /^I should see the following board$/ do |table|
+  #table is a Cucumber::Ast::Table
+  @new_grid = Grid.two_d_init(table.raw)
+  Grid.compare(@new_grid, @grid).should be_true
+end
