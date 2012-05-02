e10011508f63aeb7326a2ef5ba97a7293ad199fd
Andrew Pollok <olap84@gmail.com>
2012-05-02 21:04:28 +0100

    refactored spec and adding a test for evolve, which currently passes for this poor use case

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,14 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "GameOfLife" do
+  before(:each) do
+    @game = Game.new(3,3)
+  end
+
   it "can instantiate" do
-    game = Game.new(3,3)
-    game.grid.size.should == 9
+    @game.grid.size.should == 9
   end
 
   it "can count neighours alive" do
-     game = Game.new(3,3)
-     game.grid[[1,1]] = Cell.new(true)
-     game.count_neighbours(1,1).should == 0
+    @game.grid[[1,1]] = Cell.new(true)
+    @game.count_neighbours(1,1).should == 0
+  end
+
+  it "can evolve" do
+    @game.grid[[1,1]] = Cell.new(true)
+    @game.evolve
+    Game.compare(Game.new(3,3), @game).should be_true
   end
 end
